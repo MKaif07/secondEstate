ad02dcd558fe994fa8148f55e7329e522d892aed
Kaif <kaif2002mk@gmail.com>
2023-10-25 14:11:25 +0530

    minor change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         About Second Estate
       </h1>
       <p className="mb-4 text-slate-700">
-        Sahand Estate is a leading real estate agency that specializes in
+        Second Estate is a leading real estate agency that specializes in
         helping clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
